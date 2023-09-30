f4bda83251ed4863fbd6e9d7c664495a0211ed43
Naron <naron.chen@richmond.edu>
2023-09-30 01:55:20 -0400

    linked to firebase2

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewSpendingComponent } from './new-spending/new-spending.component';
 import { DashboardComponent } from './dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +12,8 @@ import { DashboardComponent } from './dashboard.component';
     NewSpendingComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class DashboardModule { }
--- src/app/dashboard/new-spending/new-spending.component.ts
+++ src/app/dashboard/new-spending/new-spending.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Form, FormBuilder, FormGroup } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { formatDate } from '@angular/common';
+
 
 @Component({
   selector: 'app-new-spending',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-spending.component.scss']
 })
 export class NewSpendingComponent {
+  todayDate!: string;
+  spendingForm?: FormGroup;
+
+  constructor(private fb: FormBuilder, private db: AngularFirestore){
+    const todayDate = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+    this.spendingForm = this.fb.group({
+      reason: [''],
+      amount: [''],
+      tag: [''],
+      date: [todayDate] 
+    })
+  }
+
+  onSubmit(){
+    this.db.collection('spending').add(this.spendingForm?.value).then(() => {
+      console.log('Spending Added to Database')
+      this.spendingForm?.reset();
+    })
+  }
 
 }
